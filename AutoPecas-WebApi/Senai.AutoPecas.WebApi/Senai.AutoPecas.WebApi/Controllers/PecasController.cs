489b3c08a769b3801bc13cf48f624ca329b598d9
Jônatas Augusto Masson Borges <47451835855@info.local>
2019-09-04 11:29:51 -0300

    up

---
+++ AutoPecas-WebApi/Senai.AutoPecas.WebApi/Senai.AutoPecas.WebApi/Controllers/PecasController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.AutoPecas.WebApi.Domains;
+using Senai.AutoPecas.WebApi.Repositories;
+
+namespace Senai.AutoPecas.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class PecasController : ControllerBase
+    {
+        PecaRepository PecaRepository = new PecaRepository ();
+
+        [Authorize]
+        [HttpGet]
+
+        public IActionResult Listar()
+        {
+            return Ok(PecaRepository.Listar());
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Pecas peca)
+        {
+            try
+            {
+                PecaRepository.Cadastrar(peca);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Ocorreu um Erro: " + ex.Message });
+            }
+        }
+
+
+
+
+
+    }
+}
---
+++ AutoPecas-WebApi/Senai.AutoPecas.WebApi/Senai.AutoPecas.WebApi/Repositories/PecaRepository.cs
@@ -0,0 +1,61 @@
+﻿using Senai.AutoPecas.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.AutoPecas.WebApi.Repositories
+{
+    public class PecaRepository
+    {
+        public List<Pecas> Listar()
+        {
+            using (AutoPecasContext ctx = new AutoPecasContext())
+            {
+                return ctx.Pecas.ToList();
+            }
+        }
+
+        public void Pecas(Pecas peca)
+        {
+            using (AutoPecasContext ctx = new AutoPecasContext())
+            {
+                ctx.Pecas.Add(peca);
+                ctx.SaveChanges();
+            }
+        }
+
+        //BUSCAR POR ID
+        public Pecas BuscarPorId(int id)
+        {
+            using (AutoPecasContext ctx = new AutoPecasContext())
+            {
+                return ctx.Pecas.FirstOrDefault(x => x.PecaId == id);
+            }
+        }
+
+        //ATUALIZAR
+        public void Atualizar(Pecas peca)
+        {
+            using (AutoPecasContext ctx = new AutoPecasContext())
+            {
+                Pecas PecaBuscada = ctx.Pecas.FirstOrDefault(x => x.PecaId == peca.PecaId);
+
+                PecaBuscada.Nome = peca.Nome;
+
+                ctx.Pecas.Update(PecaBuscada);
+
+                ctx.SaveChanges();
+            }
+        }
+
+        //FALTA FAZER O DELETAR
+
+
+
+
+
+
+
+    }
+}
